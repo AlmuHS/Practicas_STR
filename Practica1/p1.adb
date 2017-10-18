f85c85b6cfc80f6b66815a38818f4d5c61c53ad9
AlmuHS <liberamenso10000@gmail.com>
2017-10-18 09:48:09 +0200

    added maximal and minimal

--- Practica1/p1.adb
+++ Practica1/p1.adb
@@ -31,9 +31,8 @@ begin
 
    declare
       reg_list: vector(1 .. size);
-      counter: Integer;
-      max_temperature: fixed_point;
-      min_temperature: fixed_point;
+      max_temperature: register;
+      min_temperature: register;
    begin
 
       for i in reg_list'range loop
@@ -47,7 +46,22 @@ begin
          reg_list(i).day.year := Integer'value(Get_Line);
       end loop;
 
-      --max_temperature := reg_list'temperature
+      max_temperature := reg_list(1);
+      min_temperature := reg_list(1);
+
+      for j in reg_list'range loop
+         if max_temperature.temperature < reg_list(j).temperature then
+            max_temperature := reg_list(j);
+         end if;
+      end loop;
+
+      for k in reg_list'range loop
+         if min_temperature.temperature < reg_list(k).temperature then
+            min_temperature := reg_list(k);
+         end if;
+      end loop;
+
+      Put_Line("Minimal Temperature: " & min_temperature.temperature'img & "celsius degree, " & "reached the " & min_temperature.day.day'img & " of " & min_temperature.day.month'img & " of " & min_temperature.day.year'img);
 
 
    end;
