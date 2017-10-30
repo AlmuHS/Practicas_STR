2ea5ab1eb13e82e77b29897f32a2e4104ebd1e64
AlmuHS <liberamenso10000@gmail.com>
2017-10-30 10:07:20 +0100

    Finished Practica 1

---
+++ Practica1/p1-c.adb
@@ -0,0 +1,12 @@
+with Text_IO; use Text_IO;
+
+procedure p1 is
+begin
+   Put_Line("Littlest Integer number: " & Integer'first'img);
+   Put_Line("Biggest Integer number: " & Integer'last'img);
+   Put_Line("Littlest Natural number: " & Natural'first'img);
+   Put_Line("Biggest Natural number: " & Natural'last'img);
+   Put_Line("Float significative numbers: " & Float'digits'img);
+
+
+end p1;
