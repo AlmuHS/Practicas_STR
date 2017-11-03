da233d52665a738ca8129d88082244b41300393f
AlmuHS <liberamenso10000@gmail.com>
2017-11-03 23:31:23 +0100

    Fixed error p1-c

--- Practica1/p1-c.adb
+++ Practica1/p1-c.adb
@@ -5,8 +5,7 @@ begin
    Put_Line("Littlest Integer number: " & Integer'first'img);
    Put_Line("Biggest Integer number: " & Integer'last'img);
    Put_Line("Littlest Natural number: " & Natural'first'img);
-   Put_Line("Biggest Natural number: " & Natural'last'img);
+   Put_Line("Littlest Positive number: " & Positive'first'img);
    Put_Line("Float significative numbers: " & Float'digits'img);
-
 
 end p1;
