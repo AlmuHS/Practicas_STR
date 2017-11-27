7ce0973e1e5c3eb5f7e43ab9ff718838c02a0807
AlmuHS <liberamenso10000@gmail.com>
2017-11-27 13:36:48 +0100

    Añadida licencia

--- Practica2/colas.ads
+++ Practica2/colas.ads
@@ -1,3 +1,21 @@
+
+-- Copyright (C) 2017 Almudena García Jurado-Centurión
+
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+ 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+with Ada.Unchecked_Deallocation;
+
 generic
    type Elementos is private;
 
@@ -9,6 +27,7 @@ package Colas is
    function Esta_Llena (La_Cola: Cola) return Boolean;
    procedure Copiar (Origen: Cola; Destino:in out Cola);
    function "="(La_Cola, Con_La_Cola: Cola) return Boolean;
+
 private
 
    type register;
@@ -25,5 +44,8 @@ private
          first: link;
          last: link;
       end record;
+
+   procedure Liberar_Register is new Ada.Unchecked_Deallocation
+     (register, link);
 
 end Colas;
